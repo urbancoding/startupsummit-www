552ce31e20961adb6423526fad2257060064fb4d
Swapna Gupta <swapna.sony@gmail.com>
2013-01-30 23:34:58 -0600

    Addedshare on  twitter and facebook scripts

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-$(function(){
+$(function () {
     $('a[rel="colorbox"]').colorbox({current:"{current} of {total}", opacity:0.7});
+
+    function share(addr, windowName) {
+        var width = 575,
+            height = 400,
+            left = ($(window).width() - width) / 2,
+            top = ($(window).height() - height) / 2,
+            url = encodeURI(addr),
+            opts = 'status=1' +
+                ',width=' + width +
+                ',height=' + height +
+                ',top=' + top +
+                ',left=' + left;
+        window.open(url, windowName, opts);
+
+    }
+
+    $('.twitter').click(function (event) {
+        var tweettext = "Join local startup founders for a free day of talks inspiring next generation of high-growth companies in Birmingham.";
+        share("http://twitter.com/share?text=" + tweettext + "&url=http://www.startupsummit.org", "twitter");
+        return false;
+    });
+    $('.facebook').click(function () {
+       share("http://www.facebook.com/sharer/sharer.php?u=http://www.startupsummit.org", "facebook");
+    });
 });
